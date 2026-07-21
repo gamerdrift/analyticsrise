d932e78bfd7fa9c2db7bfb84050dbeb3df1fd909
GamerDrift <email.jafer@gmail.com>
2026-07-21 17:51:17 +0530

    Deploy production build for AnalyticsRise launch incident

---
+++ app/tableau-studio/components/layout/CenterPanel.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react';
+
+export default function CenterPanel() {
+  return (
+    <div className="flex-1 p-4 bg-gray-900/50 rounded-md">
+      Center Panel Placeholder
+    </div>
+  );
+}
